95b6a54ecf55697c9c22ce1dd60b742884ddd926
Murli <murli@belgiumwebnet.com>
2023-06-21 17:20:17 +0530

    chngs

--- frontity.settings.js
+++ frontity.settings.js
@@ -34,7 +34,34 @@ const settings = {
     },
     "@frontity/tiny-router",
     "@frontity/html2react",
-    "frontity-contact-form-7"
+    "@frontity/head-tags",
+    "@frontity/wp-comments",
+    {
+      name: "@frontity/google-analytics",
+      state: {
+        googleAnalytics: {
+          // trackingId: "UA-311655314",
+        },
+      },
+    },
+    {
+      name: "@frontity/google-tag-manager-analytics",
+      state: {
+        googleTagManagerAnalytics: {
+          // containerId: "G-D44D3408K9",
+        },
+      },
+
+
+    },
+    {
+      name: "frontity-microsoft-clarity",
+      state: {
+        microsoftClarity: {
+          // trackingCode: "emc50vc0un"
+        }
+      }
+    }
   ]
 };
 
